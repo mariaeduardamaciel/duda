e681b4c5236c93bbcc526943c90eaf0f72c51376
mariaeduardamaciel <fcollette8@gmail.com>
2024-11-25 20:23:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    'Quantos minutos tem 1 hora?',
+    '60 minutos'
+)
+
+criaCartao(
+    'Geral',
+    'Qual a capital do Brasil?',
+    'Brasilia '
+)
+
+criaCartao(
+    'Geral',
+    'Quanto é 7x8?',
+    '56'
+)
+
+criaCartao(
+    'Geral',
+    'Qual é o elemento químico representado pelo símbulo H?',
+    'Hidrogênio'
+)
